b326062ad5df5c178e057c9f90d7c305e64bcd68
Raul Pavani <raul.pavani@hotmail.com>
2025-02-20 21:57:49 -0300

    Comments and split files

---
+++ Grafos/Graph.cs
@@ -0,0 +1,75 @@
+﻿public class Graph
+{
+    //Numero de vertices que compoe o grafo
+    private int numVertices;
+    //Lista de adjacência - Grafo representado em uma lista de listas
+    //(Cada vertice, tem uma lista de vizinhos adjacentes)
+    public List<List<int>> adjList = new List<List<int>>();
+
+    //Representação das arestas em forma de matriz de adjacência
+    public bool[,] adjMatrix;
+
+
+    /// <summary>
+    /// Construtor do Grafo
+    /// </summary>
+    /// <param name="vertices">Numero de vertices que compoe o grafo</param>
+    public Graph(int vertices)
+    {
+        numVertices = vertices;
+        adjMatrix = new bool[vertices, vertices];
+
+        for (int i = 0; i < numVertices; i++)
+        {
+            adjList.Add(new List<int>());
+        }
+    }
+
+    /// <summary>
+    /// Adiciona arestas entre os vertices do grafo
+    /// </summary>
+    /// <param name="src">Vertice Origem</param>
+    /// <param name="dest">Vertice Destino</param>
+    public void AddEdge(int src, int dest)
+    {
+        adjList[src].Add(dest);
+        adjList[dest].Add(src);
+
+        adjMatrix[src, dest] = true;
+        adjMatrix[dest, src] = true;
+    }
+
+    /// <summary>
+    /// Realiza busca em largura na lista de adjacência
+    /// </summary>
+    /// <param name="startVertice">Vertice para começar a busca</param>
+    public void BreadthFirstSearch(int startVertice)
+    {
+        //Vetor dos vertices - se foram visitados ou nao
+        bool[] visited = new bool[numVertices];
+        //Fila de vizinhos
+        Queue<int> queue = new Queue<int>();
+
+        //Marca o nosso vertice inicial como visitado,
+        //e adiciona na fila para visitar os vizinhos dele
+        visited[startVertice] = true;
+        queue.Enqueue(startVertice);
+
+        while (queue.Count > 0)
+        {
+            //Pega o primeiro elemento da fila
+            int currentVertex = queue.Dequeue();
+            Console.Write(currentVertex + " ");
+
+            //Visita dos vizinhos desse elemento e adiciona na fila
+            foreach (int adj in adjList[currentVertex])
+            {
+                if (!visited[adj])
+                {
+                    visited[adj] = true;
+                    queue.Enqueue(adj);
+                }
+            }
+        }
+    }
+}
--- Grafos/Program.cs
+++ Grafos/Program.cs
@@ -2,8 +2,10 @@
 {
     static void Main()
     {
+        //Cria um novo grafo
         Graph graph = new Graph(5);
 
+        //Adiciona as conexoes do grafo (arestas)
         graph.AddEdge(0, 1);
         graph.AddEdge(0, 2);
         graph.AddEdge(1, 2);
@@ -11,58 +13,8 @@
         graph.AddEdge(2, 4);
         graph.AddEdge(3, 4);
 
+        //Realiza a busca BFS
         graph.BreadthFirstSearch(0);
-    }
-
-    class Graph
-    {
-        private int numVertices;
-        public List<List<int>> adjList = new List<List<int>>();
-        public bool[,] adjMatrix;
-
-        public Graph(int vertices)
-        {
-            numVertices = vertices;
-            adjMatrix = new bool[vertices, vertices];
-
-            for (int i = 0; i < numVertices; i++)
-            {
-                adjList.Add(new List<int>());
-            }
-        }
-
-        public void AddEdge(int src, int dest)
-        {
-            adjList[src].Add(dest);
-            adjList[dest].Add(src);
-
-            adjMatrix[src, dest] = true;
-            adjMatrix[dest, src] = true;
-        }
-
-        public void BreadthFirstSearch(int startVertice)
-        {
-            bool[] visited = new bool[numVertices];
-            Queue<int> queue = new Queue<int>();
-
-            visited[startVertice] = true;
-            queue.Enqueue(startVertice);
-
-            while(queue.Count > 0)
-            {
-                int currentVertex = queue.Dequeue();
-                Console.Write(currentVertex + " ");
-
-                foreach (int adj in adjList[currentVertex])
-                {
-                    if (!visited[adj])
-                    {
-                        visited[adj] = true;
-                        queue.Enqueue(adj);
-                    }
-                }
-            }
-        }
     }
 }
 
